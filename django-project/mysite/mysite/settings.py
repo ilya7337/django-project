2477c5ae1e78e19a3cdc1bd9c062b6f21ee5c24d
ilya7337 <akhtymov_ilya@mail.ru>
2025-01-14 16:13:12 +0500

    final commit

--- django-project/mysite/mysite/settings.py
+++ django-project/mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mo%jvvm@es^68!=_d%dx-@1hj9*u0q^j4*!smf1w9g6s8lur%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
