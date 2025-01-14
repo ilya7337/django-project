6bb45ff5a35fbbb3043983e7838c0e2713ce5356
ilya7337 <akhtymov_ilya@mail.ru>
2025-01-14 17:54:24 +0500

    final commit

--- django-project/mysite/backDev/top_skills.py
+++ django-project/mysite/backDev/top_skills.py
@@ -4,4 +4,5 @@ from django.http import HttpRequest
 
 def get_top_skills(request: HttpRequest, title, year):
     data = TopSkills.objects.filter(year=year, title=title).values('image', 'table')
+    print(data)
     return JsonResponse(list(data), safe=False)
